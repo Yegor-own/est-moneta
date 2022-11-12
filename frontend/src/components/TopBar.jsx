77a5454b31ae6eebab4969d725459a99fb06698c
Dmitriy Gorbunov <dmitrgor1@yandex.ru>
2022-11-12 17:29:39 +0500

    Add: добавлены стили TopBar и SignIn

--- frontend/src/components/TopBar.jsx
+++ frontend/src/components/TopBar.jsx
@@ -3,9 +3,9 @@ import logo from "../img/logo.svg"
 
 export default function TopBar() {
   return (
-    <div className='hidden sm:flex sm:align-middle'>
-        <img src={logo} alt=""  className='w-[82.5px] h-[20px] ml-[34.5px]'/>
-        <p className='font-head text-darkGray text-[20px] ml-[1082spx]'>Сервис сбора донатов</p>
+    <div className='hidden sm:flex sm:align-middle sm:bg-white sm:rounded-b-[25px] sm:h-[52px]'>
+        <img src={logo} alt=""  className='w-[82.5px] h-[20px] ml-[34.5px] mt-[16px]'/>
+        <p className='font-head text-darkGray text-[20px] ml-[1082px] mt-[5px]'>Сервис сбора донатов</p>
     </div>
   )
 }
--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import TopBar from '../components/TopBar'
 
 export default function SignIn() {
   return (
-    <div>
+    <div className='bg-[#F3F3F3]'>
         <TopBar/>
     </div>
   )
